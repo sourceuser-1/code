c3a704ff898618b008f687b065067c735e32d65f
sourceuser-1 <123603131+sourceuser-1@users.noreply.github.com>
2023-08-21 17:08:46 -0700

    Update __init__.py

--- drl/agents/__init__.py
+++ drl/agents/__init__.py
@@ -1,10 +1,6 @@
 from drl.agents.registration import register, make
 
 
-register(
-    id='TorchQR_DDPG-v0',
-    entry_point = 'drl.agents.drl_agents:Torch_QR_DDPG'
-)
 
 register(
     id='Torch_QR_EVT-v1',
@@ -13,26 +9,7 @@ register(
 
 
 register(
-    id='Torch_QR_model_based_EVT-v1',
-    entry_point = 'drl.agents.drl_agents:Torch_QR_model_based_EVT_v1'
-)
-
-register(
-    id='TorchQR_TD3-v0',
-    entry_point = 'drl.agents.drl_agents:Torch_QR_TD3'
-)
-
-register(
     id='Torch_QR_TD3_EVT-v1',
     entry_point = 'drl.agents.drl_agents:Torch_QR_TD3_EVT_v1'
 )
 
-register(
-    id='Torch_QR_SAC-v1',
-    entry_point = 'drl.agents.drl_agents:Torch_QR_SAC_v1'
-)
-
-register(
-    id='Torch_wcpg-v1',
-    entry_point = 'drl.agents.drl_agents:Torch_WCPG'
-)
